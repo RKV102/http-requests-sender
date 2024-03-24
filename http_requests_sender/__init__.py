18e724e485758be8446ca6ce1719bb5c0dd18e58
RKV102 <roman.krilow@mail.ru>
2024-03-24 17:38:18 +0500

    Удаление неиспользующейся get_files_content()

--- http_requests_sender/__init__.py
+++ http_requests_sender/__init__.py
@@ -8,11 +8,3 @@ INPUT_DIR = join(dirname(dirname(abspath(__file__))), 'input')
 def get_files(input_dir=INPUT_DIR):
     items = [join(input_dir, item) for item in listdir(input_dir)]
     return [item for item in items if isfile(item)]
-
-
-def get_files_content(file_paths):
-    contents = []
-    for file_path in file_paths:
-        with open(file_path) as file:
-            contents.append(file.read())
-    return contents
