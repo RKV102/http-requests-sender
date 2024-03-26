7136c56e243a943fb23047823816a0c3ab0d515b
RKV102 <roman.krilow@mail.ru>
2024-03-27 01:11:39 +0500

    Небольшой рефакторинг имён переменных

--- http_requests_sender/scripts/entry_point.py
+++ http_requests_sender/scripts/entry_point.py
@@ -4,12 +4,12 @@ from http_requests_sender.cli import parse
 
 
 def main():
-    input_dir, host, first_request_num, second_request_num = parse()
+    input_dir, host, first_request_num, last_request_num = parse()
     files_paths = get_files_paths(input_dir)
     contents = get_files_contents(files_paths)
     send_http_requests(contents=contents, host=host,
                        first_request_num=first_request_num,
-                       second_request_num=second_request_num)
+                       last_request_num=last_request_num)
 
 
 if __name__ == '__main__':
